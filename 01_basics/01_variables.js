4a47869fe3715796f0f16791bdd3fe20016e5774
shaurya sharma <shaurya.1229@gmail.com>
2026-06-10 13:15:34 +0000

    datatypes and variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,21 @@
+const accountId = 12334
+let accountEmail = "shaurya.2323@google.com"
+var accountPassword = "223123"
+accountCity = "jaipur"  // it can be declare like this.
+
+
+
+accountEmail = "hchc@google.com"
+accountPassoword = "212121221"
+accountCity = "New delhi"
+
+
+console.table([accountEmail, accountPassword, accountId, accountCity, accountState])
+
+
+/*
+prefer not to use var 
+because of issue in block scope and functional scope
+
+
+*/
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,24 @@
+" use strict " // treat all JS code as newer versions 
+
+// alert ( 3 + 3 ) // we are using nodejs, not browser
+
+let name = "shaurya"
+let age = 18 
+let isLoggedIn = false 
+
+// number = 2 to power 53
+// bigint 
+// string = ""
+// boolean = true or false 
+// null = standalone value or empty value 
+// undefined = value is not assigned 
+// symbol = unique 
+
+
+// object 
+
+console.log(typeof undefined);
+console.log(typeof null);
+
+
+
