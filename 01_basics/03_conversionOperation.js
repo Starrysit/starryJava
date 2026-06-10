e950aab270d30160a364cd7d60f7a7e41f16bd12
shaurya sharma <shaurya.1229@gmail.com>
2026-06-10 13:43:50 +0000

    conversion of data types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,39 @@
+// let score = 33
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// this will show number
+
+
+// let score = "33abc"
+// console.log(typeof score);
+// console.log(typeof (score)); // this will show string.
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" = 33
+// "33abc" = NaN 
+// true = 1  
+// false = 0
+
+
+let isLoggedIn = 1
+ 
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 = true 
+// 0 = false 
+// " " = false 
+// string = true 
+
+
+
+
+
+
+
+
