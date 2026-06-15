56225480987d73073d32481beba2a3ad21d78787
Starrysit <shaurya.1229@gmail.com>
2026-06-15 13:34:15 +0530

    feat: add foundational modules for JavaScript numbers, math operations, and date handling

---
+++ 01_basics/06_nums_maths.js
@@ -0,0 +1,34 @@
+const score = 400
+console.log(score);
+
+const balance = new Number(100);
+console.log(balance);
+
+console.log(balance.toString());
+console.log(balance.toFixed(1));
+
+const anotherBalance = 123.456789;
+console.log(anotherBalance.toPrecision(3));
+
+const hundreds = 1000000;
+// console.log(hundreds.toLocaleString('en-In'));
+
+// .......................... MATHS .....................................
+
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.6));
+console.log(Math.ceil(4.2));
+console.log(Math.floor(4.9));
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 01_basics/07_datesinJs.js
@@ -0,0 +1,25 @@
+// Date 
+
+let myDate = new Date();
+// console.log(myDate.toString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toTimeString());
+// console.log(myDate.toLocaleTimeString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toDateString());
+
+
+let mycreatedDate = new Date("01-14-2023");
+// console.log(mycreatedDate.toLocaleString());
+
+// let mytimeStamp = Date.now();
+// console.log(mytimeStamp);
+// console.log(Math.floor(Date.now() / 1000));
+
+let newDate = new Date();
+console.log(newDate.getMonth());
+console.log(newDate.getMonth() + 1);
+
+
