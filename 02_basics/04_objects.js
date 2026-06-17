7ff80ac729a8a161cbae36fc27a43ae7e90f2571
Starrysit <shaurya.1229@gmail.com>
2026-06-17 22:08:33 +0530

    feat: add tutorial code for JavaScript object creation and manipulation

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,28 @@
+const tinderUser = new Object();
+
+tinderUser.id = "123"
+tinderUser.name = "shaurya"
+tinderUser.isLoggedIn = false
+
+// console.log(tinderUser);
+
+const regularUser = {
+    email: "some@gmail.com",
+    fullname: {
+        userfullname: {
+            firstname: "shaurya",
+            lastname: "sharma"
+        }
+    }
+}
+
+// console.log(regularUser.fullname.userfullname.firstname);
+
+
+const obj1 = { 1: "a", 2: "b" }
+const obj2 = { 3: "c", 4: "d" }
+
+// const obj3 = { obj1, obj2 }
+const obj3 = Object.assign({}, obj1, obj2)
+console.log(obj3);
+
