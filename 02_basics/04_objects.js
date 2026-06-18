f2c3e4c70299f73e6a755f887b559f021bdb5f37
Starrysit <shaurya.1229@gmail.com>
2026-06-18 20:59:38 +0530

    feat: add object manipulation examples and user data array to objects.js

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -24,5 +24,31 @@ const obj2 = { 3: "c", 4: "d" }
 
 // const obj3 = { obj1, obj2 }
 const obj3 = Object.assign({}, obj1, obj2)
-console.log(obj3);
+// console.log(obj3);
+
+const users = [
+    {
+        id: 1,
+        email: "hitesh@gmail"
+    },
+
+    {
+        id: 1,
+        email: "hitesh@gmail"
+    },
+]
+
+users[1].email;
+console.log(tinderUser);
+
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+
+
+
+
+
 
