b8947977e292227e84455680843cce91aac2b386
Starrysit <shaurya.1229@gmail.com>
2026-06-20 13:29:17 +0530

    feat: implement addTwoNumbers and loginUserMessage functions for basic arithmetic and string formatting

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,39 @@
+
+// function sayMyName() {
+//     console.log("S");
+//     console.log("H");
+//     console.log("A");
+//     console.log("U");
+//     console.log("R");
+//     console.log("Y");
+//     console.log("A");
+// }
+
+// sayMyName()
+
+
+// function addTwoNumbers(number1, number2) {
+//     console.log(number1 + number2);
+// }
+
+function addTwoNumbers(number1, number2) {
+
+    // let result = number1 + number2
+    // console.log("shaurya");
+    // return result
+
+    return number1 + number2;
+}
+
+
+const result = addTwoNumbers(3, 5)
+// console.log("Result: ", result);
+
+function loginUserMessage(username) {
+
+    return `${username} just logged in`
+}
+
+// console.log(loginUserMessage("shaurya"))
+console.log(loginUserMessage("shaurya"))
+
