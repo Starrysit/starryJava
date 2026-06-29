b9a4c41226b0dd363c8571d73e939a002984d890
Starrysit <shaurya.1229@gmail.com>
2026-06-29 17:30:19 +0530

    feat: add implementation of Immediately Invoked Function Expression (IIFE)

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,11 @@
+
+// Immediately invoked function expression (IIFE)
+
+(function chai() {
+    console.log(`DB CONNECTED`);
+})()
+
+    (() => {
+        console.log(`DB CONNECTED TWO`);
+    })()
+
