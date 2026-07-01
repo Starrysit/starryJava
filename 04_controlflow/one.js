4e9842ba7df4a3f08514e264e90b0f0e30e95b69
Starrysit <shaurya.1229@gmail.com>
2026-07-01 13:11:42 +0530

    feat: add control flow examples for if statements and switch cases

---
+++ 04_controlflow/one.js
@@ -0,0 +1,50 @@
+// if
+
+// const isUserloggedIn = true
+// const temperature = 41
+
+
+// if (temperature < 50) {
+//     console.log("temperature is less than 50");
+// }
+// else {
+//     console.log("temperature > 40");
+// }
+// console.log("execute");
+// <, >, <=, >=, ==, !=, ===, !==
+
+
+////////////////////////////////////////////////////////////////////////////////////////////
+
+// const score = 200
+
+// if (score > 100) {
+//     const power = "fly"
+//     console.log(`user power: ${power}`);
+// }
+// console.log(`user power: ${power}`);
+
+
+const balance = 1000;
+
+// if(balance > 500 )console.log("test"),console.log("test2");
+
+// if (balance < 500) {
+//     console.log("less than");
+
+// } else if (balance < 750) {
+//     console.log("less than 750");
+// } else if (balance < 1000) {
+//     console.log("less than 1000");
+// } else {
+//     console.log("less than 1500");
+// }
+
+
+
+const userLoggedIn = true;
+const debitCard = true;
+
+if (userLoggedIn && debitCard) {
+    console.log("Allowed to shop");
+}
